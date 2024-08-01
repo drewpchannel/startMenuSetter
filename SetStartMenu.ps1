57bff6101cedbbb5f40374847e595009052affe6
Drew Poulin <drewpcchannel@gmail.com>
2024-08-01 13:45:24 -0400

    fixed errors and downloads from github

--- SetStartMenu.ps1
+++ SetStartMenu.ps1
@@ -1,15 +1,20 @@
-﻿#get machine username, user has to be logged in or the hive might not be mounted
-$SID = & ".\SIDget\GetSID.ps1"
-Write-Host $SID
+﻿#get files from GitHub
+Invoke-WebRequest https://github.com/drewpchannel/startMenuSetter/archive/refs/heads/main.zip -OutFile .\sms.zip
+Expand-Archive .\sms.zip
+
+#get machine username, user has to be logged in or the hive might not be mounted
+$SID = & ".\sms\startMenuSetter-main\SIDget\GetSID.ps1"
+
+$filePath = Resolve-Path ".\sms\startMenuSetter-main"
+
 #Start menu editing
-add-content 'C:\treeline_files\NewHireMenu.xml' $StartLayoutStr
-Import-StartLayout -LayoutPath "C:\treeline_files\NewHireMenu.xml" -MountPath C:\
-New-Item -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows" -Name Explorer -ErrorAction SilentlyContinue
-Reg Add "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" /V LockedStartLayout /T REG_DWORD /D 1 /F
-Reg Add "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" /V StartLayoutFile /T REG_EXPAND_SZ /D 'C:\treeline_files\NewHireMenu.xml' /F
-Stop-Process -ProcessName explorer
+Import-StartLayout -LayoutPath "$filePath\NewHireMenu.xml" -MountPath C:\
+New-Item -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows" -Name Explorer
+Reg Add "HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" /V LockedStartLayout /T REG_DWORD /D 1 /F
+Reg Add "HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" /V StartLayoutFile /T REG_EXPAND_SZ /D "$filePath\NewHireMenu.xml" /F
+Stop-Process -ProcessName explorer -Force
 Start-Sleep -s 10
 #sleep is to let explorer finish restart b4 deleting reg keys
 Remove-ItemProperty -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "LockedStartLayout" -Force
 Remove-ItemProperty -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "StartLayoutFile" -Force
-Stop-Process -ProcessName explorer
+Stop-Process -ProcessName explorer -Force
