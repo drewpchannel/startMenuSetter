0d5184b4616bdf0145f7396560a671a8b55ca546
Drew Poulin <drewpcchannel@gmail.com>
2024-08-01 17:03:58 -0400

    more checks

--- SetStartMenu.ps1
+++ SetStartMenu.ps1
@@ -10,7 +10,7 @@ if (Test-Path -Path .\sms.zip)
 
 #get files from GitHub
 Invoke-WebRequest https://github.com/drewpchannel/startMenuSetter/archive/refs/heads/main.zip -OutFile .\sms.zip
-Expand-Archive .\sms.zip
+Expand-Archive .\sms.zip -Force
 
 #get machine username, user has to be logged in or the hive might not be mounted
 $SID = & ".\sms\startMenuSetter-main\SIDget\GetSID.ps1"
@@ -19,7 +19,11 @@ $filePath = Resolve-Path ".\sms\startMenuSetter-main"
 
 #Start menu editing
 Import-StartLayout -LayoutPath "$filePath\NewHireMenu.xml" -MountPath C:\
-New-Item -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows" -Name Explorer
+if (-Not (Test-Path -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows"))
+{
+    New-Item -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows" -Name Explorer
+}
+
 Reg Add "HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" /V LockedStartLayout /T REG_DWORD /D 1 /F
 Reg Add "HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" /V StartLayoutFile /T REG_EXPAND_SZ /D "$filePath\NewHireMenu.xml" /F
 Stop-Process -ProcessName explorer -Force
@@ -29,5 +33,4 @@ Remove-ItemProperty -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft
 Remove-ItemProperty -Path "Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "StartLayoutFile" -Force
 Stop-Process -ProcessName explorer -Force
 
-Remove-Item -Path .\sms -Recurse
 Remove-Item -Path .\sms.zip
